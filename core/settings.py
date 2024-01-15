d83950369b8f402fb08f32adb418a56971674fa7
SKW1zy12 <lohmuhamatasiz@gmail.com>
2024-01-15 11:09:02 +0600

    q

--- core/settings.py
+++ core/settings.py
@@ -27,7 +27,7 @@ load_dotenv(BASE_DIR / '.env')
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get('DEBUG')
 
 ALLOWED_HOSTS = []
 
